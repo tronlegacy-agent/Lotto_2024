0252120955174fd7ac8bf43a3f8c84685b6669d8
zoe12378 <uqamdu@gmail.com>
2024-02-28 14:46:29 +0800

    加入螢幕觸控

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -2,8 +2,10 @@ package tw.edu.pu.csim.tcyang.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,4 +21,8 @@ class MainActivity : AppCompatActivity() {
         txv.text = (1..100).random().toString()
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Toast.makeText(this, "螢幕觸控", Toast.LENGTH_SHORT).show();
+        return true
+    }
 }
